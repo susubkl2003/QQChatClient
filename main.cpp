5fd91a0d6920b615dd59818a9a3aae9349fa727f
susubkl2003 <153338865+susubkl2003@users.noreply.github.com>
2024-06-23 20:29:11 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+
+#include "myapp.h"
+#include "global.h"
+#include "loginwidget.h"
+#include "databasemagr.h"
+#include "picturecutdialog.h"
+
+#include <QApplication>
+#include <QHostInfo>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setQuitOnLastWindowClosed(false);
+
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF8"));
+
+    MyApp::InitApp(a.applicationDirPath());
+    myHelper::setStyle("default");
+    // 加载数据库
+    DataBaseMagr::Instance()->OpenUserDb(MyApp::m_strDatabasePath + "user.db");
+    DataBaseMagr::Instance()->OpenMessageDb(MyApp::m_strDatabasePath + "msg.db");
+
+//    MainWindow w;
+    LoginWidget w;
+    myHelper::FormInCenter(&w);
+    w.show();
+
+    return a.exec();
+}
